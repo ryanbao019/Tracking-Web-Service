b8888b90d557b746c9187879c534a8b513141f68
ryanbao019 <145908884+ryanbao019@users.noreply.github.com>
2024-11-20 21:51:44 +1030

    Update models.py

--- loginapp/models.py
+++ loginapp/models.py
@@ -5,22 +5,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 import random
-'''
-class EmailVerificationCode(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    code = models.CharField(max_length=4)
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_verified = models.BooleanField(default=False)
-
-    @staticmethod
-    def generate_code():
-        return str(random.randint(1000, 9999))
-
-    def is_expired(self):
-        time_difference = timezone.now() - self.created_at
-        return time_difference.total_seconds() > 300  # 5 minutes in seconds
-'''
-
 from django.db import models
 from django.contrib.auth.models import User
 import random
